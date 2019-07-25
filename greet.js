4c2335d810707ed8210425ce34397bb188e94d69
Inganathi77 <gcinokuhle77@gamila.com>
2019-07-25 18:10:53 +0200

    added few tests

--- greet.js
+++ greet.js
@@ -2,27 +2,32 @@ function MyFactoryFunction(nameList){
 var greetedNames = nameList || {};
 
 function greet(name,language){
-    var y =   /^\d*\.?\d*$/.test(name)
+  if(!name) {
+    return "please enter the name or select the language";
+  }
 
-  var nameCaps = name.toUpperCase().charAt(0) + name.slice(1);
-  if(y === false){
+  if(!language) return language;
+
+    var y =   /^\d*\.?\d*$/.test(name)
+    var nameCaps = name.toUpperCase().charAt(0) + name.slice(1);
+    if(y === false){
     if(greetedNames[nameCaps] === undefined){
           greetedNames[nameCaps] = 0
     }
-
+    
     if(language === "English"){
       return "Hello, " + nameCaps;  
   }else if(language === "Isixhosa"){
       return "Molo, " + nameCaps;
   }else if(language === "Isizulu"){
       return "Sawubona, " + nameCaps;
-  }else{
-      return "Invalid language."
-  } 
+  }
+   
   }
   else {
-      "err"
+    return "please enter the name or select the language";
   }
+  
 }
 
  function getName(){
--- test/greet.test.js
+++ test/greet.test.js
@@ -1,13 +1,10 @@
 describe('greet' , function(){
-    it('should greet in any name and any language' , function(){
+    it('should return an error if no name or language was selected' , function(){
  
     var instances = MyFactoryFunction();
 
-        assert.equal(instances.greet('Inga' , "Isiswati"),'Invalid language.');
-        assert.equal(instances.greet('Inga' , "Isiswati"),'Invalid language.');
-        assert.equal(instances.greet('Inga' , "Tshivenda"),'Invalid language.');
-        assert.equal(instances.greet('Inga' , "Xitsonga"),'Invalid language.');
-        assert.equal(instances.greet('Inga' , "Afrikaans"),'Invalid language.');
+        assert.equal(instances.greet(),'please enter the name or select the language');
+      
     });
     it('should greet in English' , function(){
 
@@ -27,5 +24,27 @@ describe('greet' , function(){
 
         assert.equal(instances.greet('Inga' , "Isizulu"),'Sawubona, Inga');
     });
+    
+    it('should not take duplicate' , function(){
+
+       var instances = MyFactoryFunction();
+       instances.greet('Inga' , "Isizulu")
+       instances.greet('Inga' , "Isizulu")
+       instances.greet('Jason' , "Isizulu")
+       instances.greet('Jason' , "Isizulu")
+       instances.greet('Inga' , "Isizulu")
+       assert.equal(instances.getCount('Inga' , "Isizulu"),2);
+    });
+    it('should check the counter if its counting ' , function(){
+
+        var instances = MyFactoryFunction();
+        instances.greet('Inga' , "Isizulu")
+        instances.greet('Jesse' , "Isizulu")
+        instances.greet('Jason' , "Isizulu")
+        instances.greet('Kush' , "Isizulu")
+        instances.greet('Jex' , "Isizulu")
+        assert.equal(instances.getCount('Inga' , "Isizulu"),5);
+     });
+ 
 
 });
